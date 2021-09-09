16718ba47799ce9f20ca8bd96d3856b473078bbe
annaswitaj <annaswitaj.sw@gmail.com>
2021-09-09 10:19:15 +0200

    change app.css nd create dev branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
-import "./app.css"
+import "./App.css"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
